e921e5b068391de515168711fa674e3a721b9761
lipengpeng01 <lipengpeng01@58.com>
2019-12-01 17:57:04 +0800

    null return false

--- src/core/object.js
+++ src/core/object.js
@@ -4,7 +4,7 @@ function compareObject ({tpl, target}) {
   const keys = Object.keys(tpl);
   for(let index = 0, len = keys.length; index < len; index++) {
     const item = keys[index];
-    if (!target.hasOwnProperty(item) || getType(tpl[item]) !== getType(target[item])) {
+    if (target[item] === null || !target.hasOwnProperty(item) || getType(tpl[item]) !== getType(target[item])) {
       return false;
     }
     if (!this[getType(tpl[item])]({
