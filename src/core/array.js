d3b46708e62ea23d82fb4c6ae1ac42e579b70cae
lipengpeng01 <lipengpeng01@58.com>
2019-12-01 15:43:12 +0800

    数组支持全数组元素校验

--- src/core/array.js
+++ src/core/array.js
@@ -2,14 +2,20 @@ import {getType} from '../util/index'
 
 function compareArray ($cm, {tpl, target}) {
   if (tpl.length === 0 || target.length === 0) return true;
-  const tplCore = tpl[0];
-  const tarCore = target[0];
 
-  if (getType(tplCore) !== getType(tarCore)) return false;
-  return $cm[getType(tplCore)]($cm, {
-    tpl: tplCore,
-    target: tarCore
-  });
+  for (let i = 0, len = target.length; i < len; i++) {
+    let tplCore = tpl[i];
+    let tarCore = target[i];
+    if (getType(tplCore) !== getType(tarCore)) return false;
+
+    let _result = $cm[getType(tplCore)]($cm, {
+      tpl: tplCore,
+      target: tarCore
+    });
+
+    if (!_result) return _result;
+  }
+  return true;
 }
 
 export {
