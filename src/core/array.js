625efeb8b3c2f40afe8d971758e0d3e2393acbd4
lipengpeng01 <lipengpeng01@58.com>
2019-12-04 20:55:18 +0800

    fix array bug

--- src/core/array.js
+++ src/core/array.js
@@ -2,9 +2,8 @@ import {getType} from '../util/index'
 
 function compareArray ({tpl, target}) {
   if (tpl.length === 0 || target.length === 0) return true;
-
+  let tplCore = tpl[0];
   for (let i = 0, len = target.length; i < len; i++) {
-    let tplCore = tpl[i];
     let tarCore = target[i];
     if (getType(tplCore) !== getType(tarCore)) return false;
 
